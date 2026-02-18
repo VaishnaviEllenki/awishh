a2b58017d6f53ee354509fadce4ca4c76523c84f
VaishnaviEllenki <vyshuellenki1@gmail.com>
2026-02-18 22:14:25 +0530

    Change password and redirect to firstpage.html
    Updated the correct password and redirected to a new page.

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const correctPassword = "nani123"; // change if needed
+const correctPassword = "imnaniii"; // change if needed
 
 function verify() {
   const entered = document.getElementById("password").value;
@@ -6,7 +6,7 @@ function verify() {
   if (entered === correctPassword) {
     document.getElementById("loader").style.display = "flex";
     setTimeout(() => {
-      window.location.href = "index.html";
+      window.location.href = "firstpage.html";
     }, 2000);
   } else {
     document.getElementById("error").innerText = "Wrong password";
@@ -17,5 +17,6 @@ function togglePassword() {
   const field = document.getElementById("password");
   field.type = field.type === "password" ? "text" : "password";
 }
+
 
 
